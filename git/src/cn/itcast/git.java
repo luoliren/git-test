222496ce33031332a7a7c7befa1dbd76c4b48c23
luoliren <1304296685@qq.com>
2020-03-12 22:47:21 +0800

    把本地工程添加到git

---
+++ git/src/cn/itcast/git.java
@@ -0,0 +1,19 @@
+package cn.itcast;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
